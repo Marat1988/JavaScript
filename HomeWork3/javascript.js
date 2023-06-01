25fb127c31f9af811ea174a05f1480ff96164a41
Marat <108996479+Marat1988@users.noreply.github.com>
2023-06-01 15:57:29 +0200

    UPDATE

--- HomeWork3/javascript.js
+++ HomeWork3/javascript.js
@@ -3,22 +3,48 @@
 /* По материалам из видео
 https://youtu.be/YaM0CaDTshc
 https://youtu.be/7o5sRSBlKwo - на мой взгляд лучшее объяснение
+https://learn.javascript.ru/task/debounce?ysclid=lid2y857j6743694535
 */
-const f = debounce(console.log, 1000);
+
+function temp(e){
+    console.log(e+" Привет");
+}
+
+const f = debounce(temp, 1000);
 
 f(1);
 f(2);
 
 setTimeout(() => f(3), 100);
-setTimeout(() => f(4), 500);
-setTimeout(() => f(5), 1100);
+setTimeout(() => f(4), 1100);
+setTimeout(() => f(5), 1500);
 
-function debounce(callback, delay){
-    let timer;
-    return function(...args){
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            callback.apply(this, args);
-        }, delay);
-    }
+function debounce(callback, ms) {
+    let isCooldown = false;
+    return function() {
+      if (isCooldown) return;
+      callback.apply(this, arguments);
+      isCooldown = true; 
+      setTimeout(() => isCooldown = false, ms);
+    };
 }
+
+
+// function debounce (fn, ms){
+//     let timeout;
+//     return function(){
+//         const fncall = () => {fn.apply(this, arguments)}
+//         clearTimeout(timeout);
+//         timeout = setTimeout(fncall, ms);
+//     };
+// }
+
+// function debounce(callback, delay) {
+//     let timer;
+//     return function(...args){
+//         clearTimeout(timer);
+//         timer = setTimeout(() => {
+//             callback.apply(this, args);
+//         }, delay);
+//     }
+// }
