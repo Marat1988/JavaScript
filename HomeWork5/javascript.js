8e2b05d9b0d7196f27f16da48d1a024edf0b226c
Marat <pirat03071988@mail.ru>
2023-06-04 20:09:08 +0200

    UPDATE

--- HomeWork5/javascript.js
+++ HomeWork5/javascript.js
@@ -1,37 +1,56 @@
 'use strict';
 
 let road = document.querySelector('.track');
-let btnStart = document.getElementById('start');
+let cars= document.querySelectorAll('.car');
 
-let Max = document.querySelector('.Max');
-console.log(Max);
+const distance = road.clientWidth; //Расстояние
+const widthCar = document.querySelectorAll('.car')[0].clientWidth; //Ширина машины (по сути блока DIV). Они все одинаковые
 
-let car = document.querySelectorAll('.car');
-console.log(car);
 
-let button = document.getElementById('test');
-console.log(button);
+class Player { //Класс игрок
+    constructor(fio, car){
+        this.car = car;
+        this.fio = fio;
+        this.finished = false;
+        this.passTime= 0;
+    }
 
-button.addEventListener('click', function(){
-    console.log(`Ширина полосы ${road.clientWidth}`);
-    // let left = Max.offsetLeft;
-    // left+=50;
-    // Max.style.left = left+'px';
-    // console.log(left);
+    showInfo(){
+        return `Расстояние: ${distance} px\nВремя:${this.passTime} ms\nСкорость:${distance/this.passTime} px/ms`;
+    }
+}
 
-    let start = Date.now();
-    let timer = setInterval(function() {
-        let timePassed = Date.now() - start;
+let players = [new Player('Max', cars[0]),
+               new Player('Ivan', cars[1]),
+               new Player('Marat', cars[2]),
+               new Player('Daniil', cars[3]),
+               new Player('Nikita', cars[4]),
+               new Player('Daria', cars[5])]
 
-        Max.style.left = timePassed / 5 + 'px';
-
-        if (timePassed > 6000) clearInterval(timer);
-
-      }, 20);
-});
+let btnStart= document.getElementById('start'); //кнопка старта
 
 btnStart.addEventListener('click', function(event){
     event.preventDefault();
-    alert('Пока не работает');
-})
+    let start = Date.now(); //Время старта
+    let timer = setInterval(function() {
+        for (const race of players) {
+            let randomDistance = Math.floor(Math.random() * (10 - 1) + 1); //Случайное расстояние
+            if (race.finished===false){
+                race.car.style.left = (race.car.offsetLeft + randomDistance) + 'px'; //Типа машина едет
+                if ((race.car.offsetLeft + widthCar) > (distance + widthCar)) { //Пока не пересечет линию
+                    race.finished=true;
+                    race.passTime = (Date.now() - start);
+                }
+            }
+        }
+        let notFinished = players.filter(player=>player.finished===false).length; //Количество не финишировавших
+        if (notFinished===0){
+            clearInterval(timer);
+            console.log(players);
+            players.forEach(function(entry){
+                  alert(entry.showInfo());
+            });
+        }
+      }, 20);
 
+});
